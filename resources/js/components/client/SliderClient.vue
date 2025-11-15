ec02f24f25a3835342c2ef0ca8153484e265579b
Bongdepchaii <Buitrongthanh2k5@gmail.com>
2025-11-15 23:07:11 +0700

    fix banner 2

--- resources/js/components/client/SliderClient.vue
+++ resources/js/components/client/SliderClient.vue
@@ -2,7 +2,7 @@
     <div class="slider">
         <img src="../../../../public/images/slider_1.webp" alt="slider_1" class="w-100">
     </div>
-    <div class="slider">
+    <div class="slider container">
         <img src="../../../../public/images/slider_bottom.png" alt="slider_2" class="w-100">
     </div>
 </template>
